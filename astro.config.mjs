1947f0b6409a0d5933a6af485e9d1fe703d5f4a1
Sergio Zaldivar <zayser99@hotmail.com>
2025-04-16 13:09:05 -0600

    domamin

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://zayser.com",
-  base: "zayser.com",
+  base: "zayser99.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
