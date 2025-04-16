ffe7cb01d01a194d899f4e15dbf92e3967bdcbf4
Sergio Zaldivar <zayser99@hotmail.com>
2025-04-16 14:56:55 -0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://zayser.com",
-  base: "/zayser/",
   vite: {
     plugins: [tailwindcss()],
   },
