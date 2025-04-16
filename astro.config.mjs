16400b065675ed206d45e82769ca98058ea6d525
Sergio Zaldivar <zayser99@hotmail.com>
2025-04-16 13:03:47 -0600

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://zayser99.github.io",
-  base: "zayser99.github.io",
+  site: "https://zayser.com",
+  base: "zayser.com",
   vite: {
     plugins: [tailwindcss()],
   },
